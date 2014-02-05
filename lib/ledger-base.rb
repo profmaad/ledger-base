670387a3f58f9114c2c1f410545ab5401bc3c9ac
profmaad <prof.maad@lambda-bb.de>
2014-02-05 15:56:11 +0800

    Fix include paths

--- lib/ledger-base.rb
+++ lib/ledger-base.rb
@@ -1,8 +1,8 @@
-require_relative 'ledger_base/version'
+require_relative 'ledger-base/version'
 
-require_relative 'ledger_base/config'
-require_relative 'ledger_base/options'
+require_relative 'ledger-base/config'
+require_relative 'ledger-base/options'
 
-require_relative 'ledger_base/command'
-require_relative 'ledger_base/csv'
-require_relative 'ledger_base/transactions'
+require_relative 'ledger-base/command'
+require_relative 'ledger-base/csv'
+require_relative 'ledger-base/transactions'
